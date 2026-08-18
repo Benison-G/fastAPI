abff2e611dabb090fca69c24619ae169447204ab
Benison-G <sachinlg60@gmail.com>
2026-08-18 15:29:58 +0530

    CRUD opterations

--- main.py
+++ main.py
@@ -9,11 +9,39 @@ def greet():
 
 products = [
     Product(id= 1, name= "Mobile", description= "Phone device", price= 9999.99, quantity= 10),
-        Product(id= 1, name= "Laptop", description= "A computer", price= 19999.99, quantity= 12),
-            Product(id= 1, name= "Table", description= "An equipment for home", price= 999, quantity= 8),
-                Product(id= 1, name= "Utensils", description= "A kit for kitchen", price= 999, quantity= 20)
+        Product(id= 2, name= "Laptop", description= "A computer", price= 19999.99, quantity= 12),
+            Product(id= 3, name= "Table", description= "An equipment for home", price= 999, quantity= 8),
+                Product(id= 4, name= "Utensils", description= "A kit for kitchen", price= 999, quantity= 20)
 ]
 
 @app.get("/products")
 def get_all_products(): 
     return products
+
+@app.get("/products/{id}")
+def get_product_by_id(id: int):
+    for product in products:
+        if (product.id == id):
+            return product
+    return "Item not found"
+
+@app.post("/products")
+def add_product(product: Product):
+    products.append(product)
+    return product;
+
+@app.put("/products")
+def update_product(id: int, product: Product):
+    for i in range(len(products)):
+        if (products[i].id == id):
+            products[i] = product;
+            return "Product added"
+    return "Product not found"
+
+@app.delete("/products")
+def delete_product(id: int):
+    for i in range(len(products)):
+        if (products[i].id == id):
+            del products[i]
+            return "Delete success"
+    return "Product not found"    

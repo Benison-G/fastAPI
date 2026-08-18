eb1a2956bca70e23f7433dd21b951676853279c1
Benison-G <sachinlg60@gmail.com>
2026-08-18 19:02:18 +0530

    PWD update

--- database.py
+++ database.py
@@ -1,8 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-#TODO: change the password while working on local and do not password to github ${pwd}
-
-db_url = "postgresql://postgres:{pwd}@localhost:5432/fastapi-demo"
+db_url = "postgresql://postgres:1234@localhost:5432/fastapi-demo"
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
